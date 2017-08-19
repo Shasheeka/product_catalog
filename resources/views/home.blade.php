3f20b8462d65adf0db8e1295127820e5b0f07329
shasheeka <shasheeka@gmail.com>
2017-08-19 12:26:49 +0530

    new cat name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,9 +17,9 @@
         </div>
     </header>
 
-    <!-- Portfolio Grid Section -->
+
     <section id="Categories">
-        <div class="container">
+        <div class="mcontainer">
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <h2>Categories</h2>
@@ -27,25 +27,243 @@
                 </div>
             </div>
             <div class="row">
-
-
-                @foreach ($categories as $category)
-
                 <div class="col-sm-4 col-md-3 portfolio-item">
-                    <a href="{{ URL('/products/category/'.$category->id )}}" class="portfolio-link" >
+                    <a href="/products/category/1" class="portfolio-link" data-toggle="modal">
 
-                        <img src="{{'img/portfolio/category'.$category->id.'.png'}}" class="img-responsive" alt="Slice of cake">
+                        <img src="img/portfolio/category1.png" class="img-responsive" alt="Slice of cake">
                     </a>
-                    <div class="cat-name" >
-                        {{$category->name}}
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"> <p>" Joey "</p></div></div>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/2" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category2.png" class="img-responsive" alt="Circus tent">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"><p>" Platypus "</p></div></div>
 
                     </div>
                 </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/3" class="portfolio-link" data-toggle="modal">
 
-                @endforeach
+                        <img src="img/portfolio/category3.png" class="img-responsive" alt="Game controller">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"><p>" Koala"</p></div></div>
+
+                        </p>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/4" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category4.png" class="img-responsive" alt="Safe">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"><p>"Brown Snake"</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/5" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category5.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"><p> <p>" Possum "</p></p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/6" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category6.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"> <p>" Tasmanian Devil "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/7" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category7.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name">  <p>" Wombat "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/8" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category8.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"> <p>" Bilby "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/9" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category9.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"> <p>" Kookaburra "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/10" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category10.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"> <p>" Kangaroo "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/11" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category11.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"><p>" Echidna "</p></div></div>
+
+                    </div>
+
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/12" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category12.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"> <p>" Crocadile "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/13" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category13.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"><p>" Goanna "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/14" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category14.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"> <p>" Emu "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/15" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category15.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"><p>" Dingo "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/16" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category16.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"> <p>" Flying Fox "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/17" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category17.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"> <p>" Name "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/18" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category18.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"> <p>" Name "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/19" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category19.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"> <p>" Name "</p></div></div>
+
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 portfolio-item">
+                    <a href="/products/category/20" class="portfolio-link" data-toggle="modal">
+
+                        <img src="img/portfolio/category20.png" class="img-responsive" alt="Submarine">
+                    </a>
+                    <div class="caption">
+                        <div><div class="cat-name">Product Category</div> <div class="animal-name"><p>" Name "</p></div></div>
+                    </div>
+                </div>
+
+
 
             </div>
         </div>
     </section>
+    
+
+    <!-- Portfolio Grid Section -->
+    {{--<section id="Categories">--}}
+        {{--<div class="container">--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-lg-12 text-center">--}}
+                    {{--<h2>Categories</h2>--}}
+                    {{--<hr class="star-primary">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="row">--}}
+
+
+                {{--@foreach ($categories as $category)--}}
+
+                {{--<div class="col-sm-4 col-md-3 portfolio-item">--}}
+                    {{--<a href="{{ URL('/products/category/'.$category->id )}}" class="portfolio-link" >--}}
+
+                        {{--<img src="{{'img/portfolio/category'.$category->id.'.png'}}" class="img-responsive" alt="Slice of cake">--}}
+                    {{--</a>--}}
+                    {{--<div class="cat-name" >--}}
+                        {{--{{$category->name}}--}}
+
+                    {{--</div>--}}
+                {{--</div>--}}
+
+                {{--@endforeach--}}
+
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</section>--}}
 </div>
 @endsection
